f8a2467930f61ee9f1f0515a1667992551615bcb
ermi9 <dgnrys03b15z315a@studenti.unime.it>
2026-03-17 20:18:12 +0100

    summary and activity endpoints

--- src/main/java/com/ermiyas/exchange/api/controller/ActivityController.java
+++ src/main/java/com/ermiyas/exchange/api/controller/ActivityController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+//@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/api/v1/activity")
 public class ActivityController {

638ab10db7f6a3ec0b46c3c168c7bc0c50742184
ermi9 <dgnrys03b15z315a@studenti.unime.it>
2026-03-18 09:44:42 +0100

    custom identity conflict exception, to show descriptive error messages in the case where a 'standarduser' tries to login as 'Admin' or viceversa

---
+++ src/main/java/com/ermiyas/exchange/domain/exception/IdentityConflictException.java
@@ -0,0 +1,5 @@
+package com.ermiyas.exchange.domain.exception;
+
+public class IdentityConflictException extends ExchangeException {
+    public IdentityConflictException(String message) { super(message); }
+}

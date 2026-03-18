087c5732d49eb6c0b6bec5c671b5f766a328a08b
ermi9 <dgnrys03b15z315a@studenti.unime.it>
2026-03-18 09:46:57 +0100

    for throwing descriptive error messages for money related invariants violation

---
+++ src/main/java/com/ermiyas/exchange/domain/exception/InsufficientFundsException.java
@@ -0,0 +1,5 @@
+package com.ermiyas.exchange.domain.exception;
+
+public class InsufficientFundsException extends ExchangeException {
+    public InsufficientFundsException(String message) { super(message); }
+}

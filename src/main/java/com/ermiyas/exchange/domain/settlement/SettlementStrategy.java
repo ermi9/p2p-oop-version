caf411971ce5766531667804f3ccc6aff66cdbc2
ermi9 <dgnrys03b15z315a@studenti.unime.it>
2026-06-09 14:52:49 +0200

    this class now adheres with SRP, removing the self-aware marketType enum they used to contain before

--- src/main/java/com/ermiyas/exchange/domain/settlement/SettlementStrategy.java
+++ src/main/java/com/ermiyas/exchange/domain/settlement/SettlementStrategy.java
@@ -1,9 +1,7 @@
 package com.ermiyas.exchange.domain.settlement;
 
-import com.ermiyas.exchange.domain.model.MarketType;
 import com.ermiyas.exchange.domain.model.Outcome;
 
 public interface SettlementStrategy {
     Outcome determineWinner(int homeScore, int awayScore);
-    MarketType getMarketType();
 }
